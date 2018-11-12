5ec1fff69012f9c1deedf0de47aea2830aa4a0ec
M6code <benjamynfrank101@gmail.com>
2018-11-12 11:31:25 +0100

    Fixed error

--- 01 programming foundation and web/rename_files.py
+++ 01 programming foundation and web/rename_files.py
@@ -4,7 +4,7 @@ import os;
 def rename_files():
 	# 1 get file names from folder
 	file_list = os.listdir(
-		r"C:\Users\Benjamin_PC\web_dev\webdev Nanodegree_Udacity\01 pragramming foundation and web\prank")
+		r"C:\Users\Benjamin_PC\web_dev\webdev Nanodegree_Udacity\01 programming foundation and web\prank")
 	print file_list;
 
 rename_files()
