4b5832237efc538ddcda9b3a97981d529944ca66
M6code <benjamynfrank101@gmail.com>
2018-11-12 10:35:17 +0100

    Launch the webbrowser on loop

--- 01 pragramming foundation and web/break_time.py
+++ 01 pragramming foundation and web/break_time.py
@@ -1,6 +1,11 @@
 import webbrowser;
 import time;
 
-time.sleep(10); #Delay program for 10sec
-webbrowser.open("https://www.google.com"); #Launch Web browser
+breaks_no = 3;
+break_count = 0;
 
+print("This Program Started at: " + time.ctime());
+while(break_count < breaks_no):
+    time.sleep(5); #Delay program for x-sec
+    webbrowser.open("https://www.google.com"); #Launch Web browser
+    break_count = break_count + 1;
