581bb463f13ebb4bd81f3fba6f4109bea1f76d2f
M6code <benjamynfrank101@gmail.com>
2018-11-12 19:59:11 +0100

    05.12 Using Classes - Improve code quality task completed

--- 01 programming foundation and web/mind_storm.py
+++ 01 programming foundation and web/mind_storm.py
@@ -1,26 +1,50 @@
 import turtle
 
-def draw_square():
+def draw_shapes():
 	window = turtle.Screen()
 	window.bgcolor('red')
 
-	brad = turtle.Turtle()
-	brad.shape("circle")
-	brad.color("blue")
-	brad.fill(True)
-	brad.shapesize(2,2)
+	# draw a square
+	def draw_sqare():
+		brad = turtle.Turtle()
+		brad.shape("circle")
+		brad.color("blue")
+		brad.fill(True)
+		brad.speed(2)
+		line_count = 0
+		no_line = 4
+		while line_count < no_line:
+			brad.forward(100)  # Move 100 step forward
+			brad.right(90)  # turn right at 90 degree angle
+			line_count = line_count + 1
+		brad.fill(False)
 
-	brad.speed(2)
-	brad.forward(100)  # Move 100 step forward
-	brad.right(90)  # turn right at 90 degree angle
-	brad.forward(100)  # Move 100 step forward
-	brad.right(90)  # turn right at 90 degree angle
-	brad.forward(100)  # Move 100 step forward
-	brad.right(90)  # turn right at 90 degree angle
-	brad.forward(100)  # Move 100 step forward
-	brad.right(90)  # turn right at 90 degree angle
-	brad.fill(False)
+	# draw a circle with fill
+	def draw_circle():
+		angie = turtle.Turtle()
+		angie.fill("True")
+		angie.shape("arrow")
+		angie.color("yellow")
+		angie.speed(2)
+		angie.circle(100)
+		angie.fill("False")
 
+	# draw a triangle with fill
+	def draw_triangle():
+		titi = turtle.Turtle()
+		titi.fill("True")
+		titi.shape("triangle")
+		titi.color("green")
+		titi.speed(2)
+		# repeat drawing 3 times
+		for i in range(1, 4):
+			titi.forward(150)  # Move forward 150 pixel
+			titi.left(120)  # turn left at 120 degree angle
+		titi.fill("False")
+
+	draw_sqare()
+	draw_circle()
+	draw_triangle()
 	window.exitonclick()
 
-draw_square()
+draw_shapes()
