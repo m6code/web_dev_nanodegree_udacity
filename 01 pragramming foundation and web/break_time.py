af8b93cd0385e6bd8347763d3d0e78dbc1c645d3
M6code <benjamynfrank101@gmail.com>
2018-11-12 10:21:44 +0100

    add timer

--- 01 pragramming foundation and web/break_time.py
+++ 01 pragramming foundation and web/break_time.py
@@ -1,2 +1,6 @@
 import webbrowser;
-webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+import time;
+
+time.sleep(10); #Delay program for 10sec
+webbrowser.open("https://www.google.com"); #Launch Web browser
+
