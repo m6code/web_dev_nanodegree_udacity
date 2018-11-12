5fe01bd7bf60723e586c3e2590addbc3dabbb05a
M6code <benjamynfrank101@gmail.com>
2018-11-12 20:27:20 +0100

    05.16 Using Classes - Making circle out of squares and triangle

--- 01 programming foundation and web/mind_storm.py
+++ 01 programming foundation and web/mind_storm.py
@@ -10,13 +10,14 @@ def draw_shapes():
 		brad.shape("circle")
 		brad.color("blue")
 		brad.fill(True)
-		brad.speed(2)
-		line_count = 0
-		no_line = 4
-		while line_count < no_line:
-			brad.forward(100)  # Move 100 step forward
-			brad.right(90)  # turn right at 90 degree angle
-			line_count = line_count + 1
+		brad.speed(10)
+		# repeat drawing square to form circle
+		for i in range(1, 37):
+			# draw four lines to make a square
+			for j in range(1, 5):
+				brad.forward(100)  # Move 100 step forward
+				brad.right(90)  # turn right at 90 degree angle
+			brad.left(10)  # tilt created square left by 10 degree
 		brad.fill(False)
 
 	# draw a circle with fill
@@ -32,18 +33,19 @@ def draw_shapes():
 	# draw a triangle with fill
 	def draw_triangle():
 		titi = turtle.Turtle()
-		titi.fill("True")
-		titi.shape("triangle")
+		titi.shape("turtle")
 		titi.color("green")
-		titi.speed(2)
-		# repeat drawing 3 times
-		for i in range(1, 4):
-			titi.forward(150)  # Move forward 150 pixel
-			titi.left(120)  # turn left at 120 degree angle
-		titi.fill("False")
+		titi.speed(20)
+		# Repeat drawing triangle to make circle
+		for i in range(1, 37):
+			# repeat drawing line 3 times at angle 150 to create triangle
+			for j in range(1, 4):
+				titi.forward(150)  # Move forward 150 pixel
+				titi.left(120)  # turn left at 120 degree angle
+			titi.right(10)
 
 	draw_sqare()
-	draw_circle()
+	#draw_circle()
 	draw_triangle()
 	window.exitonclick()
 
