33cdb05b3d8011900ad688f1d7675092858cebe8
M6code <benjamynfrank101@gmail.com>
2018-11-12 12:03:47 +0100

    Rename files to decode hidden message

--- 01 programming foundation and web/rename_files.py
+++ 01 programming foundation and web/rename_files.py
@@ -1,10 +1,18 @@
-import os;
+import os
 
 
 def rename_files():
 	# 1 get file names from folder
 	file_list = os.listdir(
 		r"C:\Users\Benjamin_PC\web_dev\webdev Nanodegree_Udacity\01 programming foundation and web\prank")
-	print file_list;
+	print file_list  # Print the list of files in the directory
+	saved_path = os.getcwd()  # save the current working directory
+	# change the current working directory
+	os.chdir(r"C:\Users\Benjamin_PC\web_dev\webdev Nanodegree_Udacity\01 programming foundation and web\prank")
+
+	# 2 rename files in directory
+	for fname in file_list:
+		os.rename(fname, fname.translate(None,"0123456789"))
+	os.chdir(saved_path)  # Change back to current work directory
 
 rename_files()
