66cfc9a9048f909631208f4d8ea65c9e3f0d4273
M6code <benjamynfrank101@gmail.com>
2018-11-12 11:29:19 +0100

    Print list of files to console

---
+++ 01 pragramming foundation and web/rename_files.py
@@ -0,0 +1,10 @@
+import os;
+
+
+def rename_files():
+	# 1 get file names from folder
+	file_list = os.listdir(
+		r"C:\Users\Benjamin_PC\web_dev\webdev Nanodegree_Udacity\01 pragramming foundation and web\prank")
+	print file_list;
+
+rename_files()
